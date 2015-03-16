b47c46b5b3bd2d713987026260d984f15de4e60b
Philémon Favrod <phil@tsf-436-wpa-2-165.epfl.ch>
2015-03-16 19:21:50 +0100

    a basic client + application master implementation

--- src/ApplicationMaster.java
+++ src/ApplicationMaster.java
@@ -1,37 +1,48 @@
 import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.records.Container;
 import org.apache.hadoop.yarn.api.records.ContainerStatus;
 import org.apache.hadoop.yarn.api.records.NodeReport;
+import org.apache.hadoop.yarn.api.records.Priority;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.client.api.AMRMClient;
+import org.apache.hadoop.yarn.client.api.AMRMClient.ContainerRequest;
 import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
 
 /**
  * The TransactionMaster is the ApplicationMaster of this application.
  * It is started by the ResourceManager on client's request.
  * Then, it receives command directly from the client.
  */
-public class TransactionMaster implements AMRMClientAsync.CallbackHandler {
-	YarnConfiguration configuration;
-    String command;
-
-    public TransactionMaster(String command) {
-        this.command = command;
-        this.configuration = new YarnConfiguration();
-    }
+public class ApplicationMaster implements AMRMClientAsync.CallbackHandler {
+	Configuration configuration;
+	AMRMClient<ContainerRequest> rmClient;
+	String[] hosts;
     
-    public YarnConfiguration getConfiguration() {
+    public Configuration getConfiguration() {
         return configuration;
     }
 
     public static void main(String[] args) throws Exception {
-        final String command = args[0];
-
-        TransactionMaster master = new TransactionMaster(command);
+        ApplicationMaster master = new ApplicationMaster();
+        System.out.println(args);
         master.runMainLoop();
     }
 
     public void runMainLoop() throws Exception {
+    	configuration = new Configuration();
+    	
+    	// Initialize 
+    	rmClient = AMRMClient.createAMRMClient();
+    	rmClient.init(configuration);
+    	rmClient.start();
+    	
+    	System.out.println("hey");
+    	
+    	//Priority priority = Priority.newInstance(0); // intra-application priority
+    	//Resource capability = Resource.newInstance(128, 1);
+    	
     }
 
 	@Override
@@ -46,7 +57,7 @@ public class TransactionMaster implements AMRMClientAsync.CallbackHandler {
 
 	@Override
 	public void onNodesUpdated(List<NodeReport> nodeReports) {
-		
+		System.out.println(nodeReports);
 	}
 
 	@Override
--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,97 @@
+import static java.util.Arrays.asList;
+import static org.apache.hadoop.yarn.api.ApplicationConstants.LOG_DIR_EXPANSION_VAR;
+import static org.apache.hadoop.yarn.client.api.YarnClient.createYarnClient;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.yarn.api.ApplicationConstants.Environment;
+import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.LocalResource;
+import org.apache.hadoop.yarn.api.records.NodeId;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.client.api.YarnClientApplication;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
 
 public class Client {
 
+	private YarnConfiguration yarnConfiguration = null;
+	private YarnClient yarnClient = null;
+	private YarnClientApplication application = null;
 
-  public void run(String[] args) throws Exception {
-  }
+	public static void main(String[] args) throws Exception {
+		new Client().run(args);
+	}
 
-  public static void main(String[] args) throws Exception {
-  }
+	public void run(String[] args) throws YarnException, IOException {
+
+		String amJarPath = args[0];
+
+		initializeYarnClient();
+
+		// Create the YARN application
+		application = yarnClient.createApplication();
+
+		// Submit the application
+		submitApplication(new Path(amJarPath));
+	}
+
+	private void initializeYarnClient() {
+		yarnClient = createYarnClient();
+
+		yarnConfiguration = new YarnConfiguration();
+		yarnClient.init(yarnConfiguration);
+
+		yarnClient.start();
+	}
+
+	private void submitApplication(Path amJarPath) throws IOException, YarnException {
+
+		LinkedList<String> hosts = new LinkedList<String>();
+		for (NodeId nodeId : yarnClient.getNodeToLabels().keySet()) {
+			hosts.add(nodeId.getHost());
+		}
+		
+		System.out.println(hosts);
+		
+		  // Set the necessary command to execute the application master
+		Vector<CharSequence> vargs = new Vector<CharSequence>(30);
+		vargs.add(Environment.JAVA_HOME.$$() + "/bin/java");
+		vargs.add("-Xmx" + 256 + "m");
+		vargs.add("ApplicationMaster");
+		vargs.add(hosts.toString());
+		vargs.add("--container_memory 1024");
+		vargs.add("--container_cores 1");
+		vargs.add("--num_containers 3");
+		vargs.add("1>" + LOG_DIR_EXPANSION_VAR + "/TransactionMaster.stdout");
+		vargs.add("2>" + LOG_DIR_EXPANSION_VAR + "/TransactionMaster.stderr");  
+
+		
+		// Get final commmand
+		StringBuilder command = new StringBuilder();
+		for (CharSequence str : vargs) {
+		  command.append(str).append(" ");
+		}
+
+		Map<String, LocalResource> localResources = new HashMap<String, LocalResource>();
+		Map<String, String> env = new HashMap<String, String>();
+		  
+		ContainerLaunchContext containerSpec = ContainerLaunchContext.newInstance(localResources, env, asList(command.toString()), null, null, null);
+		Resource capability = Resource.newInstance(256, 1);
+		  
+		ApplicationSubmissionContext appContext = application.getApplicationSubmissionContext();
+		appContext.setQueue("trans-kv-queue");
+		appContext.setAMContainerSpec(containerSpec);
+		appContext.setResource(capability);
+		appContext.setApplicationName("Transactional Key-Value Store");
+		  
+		yarnClient.submitApplication(appContext);
+	}
 }
