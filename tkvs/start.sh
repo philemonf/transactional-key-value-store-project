3fd39afec9a56ce0c4b32cbd19b6abd2ef22df46
Philémon Favrod <phil@tsf-436-wpa-4-235.epfl.ch>
2015-03-17 13:55:43 +0100

    link jar to the containers

--- tkvs/start.sh
+++ tkvs/start.sh
@@ -13,8 +13,8 @@ fi
 
 javac -cp "$HADOOP_CP" -d classes/ `find src -name *.java`
 
-jar cvfe AppMaster.jar ch.epfl.tkvs.AppMaster -C classes/ .
-jar cvfe TransactionManagerDeamon.jar ch.epfl.tkvs.TransactionManagerDeamon -C classes/ .
+jar cvfe AppMaster.jar com.epfl.tkvs.AppMaster -C classes/ .
+jar cvfe TransactionManagerDeamon.jar com.epfl.tkvs.TransactionManagerDeamon -C classes/ .
 
 hdfs dfs -mkdir -p $HDFS_PATH
 hdfs dfs -rm -f "$HDFS_PATH/TransactionManagerDeamon.jar"
@@ -24,5 +24,5 @@ if [[ -z "$HADOOP_HOME" ]]
 then
 	hadoop jar `find $HADOOP_PREFIX -name *unmanaged-am-launcher*.jar|head -n 1` -appname 'transactional kv store' -cmd 'java com.epfl.tkvs.AppMaster' -classpath AppMaster.jar
 else
-	hadoop jar "$HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-unmanaged-am-launcher-*.jar" -appname 'transactional kv store' -cmd 'java ch.epfl.tkvs.AppMaster' -classpath AppMaster.jar
+	hadoop jar "$HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-unmanaged-am-launcher-*.jar" -appname 'transactional kv store' -cmd 'java com.epfl.tkvs.AppMaster' -classpath AppMaster.jar
 fi
