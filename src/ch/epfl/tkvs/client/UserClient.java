18a76f4c40dc91899f2f0243a4aed958a132815e
Sachin Basil John <sachin.basiljohn@epfl.ch>
2015-03-21 16:53:20 +0100

    Added communication classes, not linked with rest of project

---
+++ src/ch/epfl/tkvs/client/UserClient.java
@@ -0,0 +1,21 @@
+package ch.epfl.tkvs.client;
+
+import ch.epfl.tkvs.common.MyValue;
+import ch.epfl.tkvs.common.MyKey;
+import ch.epfl.tkvs.transactionmanager.AMServer;
+
+public class UserClient
+  {
+
+    public static void main(String[] args) throws Exception
+      {
+        System.out.println("Client starting");
+        Transaction.initialize("localhost", AMServer.portNumber);
+        MyKey k0 = new MyKey(0);
+        Transaction t = new Transaction(k0);
+        t.write(k0, new MyValue("zero"));
+        System.out.println(t.read(k0));
+        
+
+      }
+  }
