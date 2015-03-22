2c2f48340d4fcf7e0f01881d7922fea38c578d09
George Piskas <geopiskas@gmail.com>
2015-03-22 01:02:31 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -13,18 +13,19 @@ rm -r -f build
 
 # Builds the jar.
 mkdir build
-find . -name "*.java" > sources.txt
-javac -cp "lib:$HADOOP_CP" -d build @sources.txt
-rm -f sources.txt
+find . -name "*.java" > .sources
+javac -cp "lib:$HADOOP_CP" -d build @.sources
 cp -r lib/org build/
 jar cf TKVS.jar -C build .
-
+rm -f .sources
+rm -r -f build
 
 # Puts the jar in HDFS under /projects/transaction-manager/.
-hadoop fs -mkdir -p /projects/transaction-manager/config
 hadoop fs -rm -r -f "/projects/transaction-manager/*"
 hadoop fs -copyFromLocal TKVS.jar /projects/transaction-manager/TKVS.jar
-hadoop fs -copyFromLocal ./config/* /projects/transaction-manager/config/
+hadoop fs -mkdir -p /projects/transaction-manager/config
+hadoop fs -copyFromLocal config /projects/transaction-manager/
+
 # Executes the Client.
 hadoop jar TKVS.jar ch.epfl.tkvs.yarn.Client
 
