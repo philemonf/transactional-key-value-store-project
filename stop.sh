040cbe5569d0c93b0aaaa8c912a73462acefe8db
George Piskas <geopiskas@gmail.com>
2015-03-24 00:52:55 +0100

    Better scripts

--- stop.sh
+++ stop.sh
@@ -2,8 +2,4 @@
 
 # Outputs the whole log of the last app.
 last=`yarn application -list 2>/dev/null | grep -o "^application_[0-9]*_[0-9]*"`
-if [ -z "$last" ]
-then
-	last=`ls -1t $HADOOP_HOME/logs/userlogs/ | head -1`
-fi
-cat $HADOOP_HOME/logs/userlogs/"$last"/*/*
+yarn application -kill "$last"
