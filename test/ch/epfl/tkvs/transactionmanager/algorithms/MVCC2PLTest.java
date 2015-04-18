aa93db53463d4adf8bba98a0b524a1daa2032047
egeyar <egeyar@gmail.com>
2015-04-19 01:58:59 +0200

    Test Case for Deadlock -> Problem with Aborts

--- test/ch/epfl/tkvs/transactionmanager/algorithms/MVCC2PLTest.java
+++ test/ch/epfl/tkvs/transactionmanager/algorithms/MVCC2PLTest.java
@@ -7,232 +7,257 @@ import ch.epfl.tkvs.transactionmanager.communication.requests.WriteRequest;
 import ch.epfl.tkvs.transactionmanager.communication.responses.GenericSuccessResponse;
 import ch.epfl.tkvs.transactionmanager.communication.responses.ReadResponse;
 import java.io.IOException;
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.fail;
+
+import junit.framework.TestCase;
 import org.junit.Test;
 
-public class MVCC2PLTest
-  {
+public class MVCC2PLTest extends TestCase {
+	private static MVCC2PL concurrentInstance = new MVCC2PL();
 
-    @Test
-    public void testRead()
-      {
-        try
-          {
+	@Test
+	public void testRead() {
+		try {
 
-            ReadRequest request = new ReadRequest(0, 0, 0);
-            MVCC2PL instance = new MVCC2PL();
+			ReadRequest request = new ReadRequest(0, 0, 0);
+			MVCC2PL instance = new MVCC2PL();
 
-            ReadResponse result = instance.read(request);
-            assertEquals(false, result.getSuccess());
+			ReadResponse result = instance.read(request);
+			assertEquals(false, result.getSuccess());
 
-            GenericSuccessResponse br;
-            br = instance.begin(new BeginRequest(0));
-            assertEquals(true, br.getSuccess());
+			GenericSuccessResponse br;
+			br = instance.begin(new BeginRequest(0));
+			assertEquals(true, br.getSuccess());
 
-            result = instance.read(request);
-            assertEquals(true, result.getSuccess());
-            assertEquals(null, result.getValue());
+			result = instance.read(request);
+			assertEquals(true, result.getSuccess());
+			assertEquals(null, result.getValue());
 
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
-      }
+		} catch (IOException ex) {
+			fail(ex.getLocalizedMessage());
+		}
+	}
 
-    /**
-     * Test of write method, of class MVCC2PL.
-     */
-    @Test
-    public void testWrite()
-      {
+	/**
+	 * Test of write method, of class MVCC2PL.
+	 */
+	@Test
+	public void testWrite() {
 
-        try
-          {
-            WriteRequest request = new WriteRequest(0, 0, 0, 0);
-            MVCC2PL instance = new MVCC2PL();
+		try {
+			WriteRequest request = new WriteRequest(0, 0, 0, 0);
+			MVCC2PL instance = new MVCC2PL();
 
-            GenericSuccessResponse result = instance.write(request);
-            assertEquals(false, result.getSuccess());
+			GenericSuccessResponse result = instance.write(request);
+			assertEquals(false, result.getSuccess());
 
-            GenericSuccessResponse br;
-            br = instance.begin(new BeginRequest(0));
-            assertEquals(true, br.getSuccess());
+			GenericSuccessResponse br;
+			br = instance.begin(new BeginRequest(0));
+			assertEquals(true, br.getSuccess());
 
-            result = instance.write(request);
-            assertEquals(true, result.getSuccess());
+			result = instance.write(request);
+			assertEquals(true, result.getSuccess());
 
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
-      }
+		} catch (IOException ex) {
+			fail(ex.getLocalizedMessage());
+		}
+	}
 
-    /**
-     * Test of begin method, of class MVCC2PL.
-     */
-    @Test
-    public void testBegin()
-      {
+	/**
+	 * Test of begin method, of class MVCC2PL.
+	 */
+	@Test
+	public void testBegin() {
 
-        BeginRequest request = new BeginRequest(0);
-        MVCC2PL instance = new MVCC2PL();
+		BeginRequest request = new BeginRequest(0);
+		MVCC2PL instance = new MVCC2PL();
 
-        GenericSuccessResponse result = instance.begin(request);
-        assertEquals(true, result.getSuccess());
+		GenericSuccessResponse result = instance.begin(request);
+		assertEquals(true, result.getSuccess());
 
-        result = instance.begin(request);
-        assertEquals(false, result.getSuccess());
-      }
+		result = instance.begin(request);
+		assertEquals(false, result.getSuccess());
+	}
 
-    /**
-     * Test of commit method, of class MVCC2PL.
-     */
-    @Test
-    public void testCommit()
-      {
+	/**
+	 * Test of commit method, of class MVCC2PL.
+	 */
+	@Test
+	public void testCommit() {
 
-        try
-          {
-            CommitRequest request = new CommitRequest(0);
+		try {
+			CommitRequest request = new CommitRequest(0);
 
-            MVCC2PL instance = new MVCC2PL();
+			MVCC2PL instance = new MVCC2PL();
 
-            GenericSuccessResponse result = instance.commit(request);
-            assertEquals(false, result.getSuccess());
+			GenericSuccessResponse result = instance.commit(request);
+			assertEquals(false, result.getSuccess());
 
-            GenericSuccessResponse br;
-            br = instance.begin(new BeginRequest(0));
-            assertEquals(true, br.getSuccess());
+			GenericSuccessResponse br;
+			br = instance.begin(new BeginRequest(0));
+			assertEquals(true, br.getSuccess());
 
-            result = instance.commit(request);
-            assertEquals(true, result.getSuccess());
+			result = instance.commit(request);
+			assertEquals(true, result.getSuccess());
 
-            result = instance.commit(request);
-            assertEquals(false, result.getSuccess());
+			result = instance.commit(request);
+			assertEquals(false, result.getSuccess());
 
-            ReadResponse rr = instance.read(new ReadRequest(0, 0, 0));
-            assertEquals(false, rr.getSuccess());
+			ReadResponse rr = instance.read(new ReadRequest(0, 0, 0));
+			assertEquals(false, rr.getSuccess());
 
-            result = instance.write(new WriteRequest(0, 0, 0, 0));
-            assertEquals(false, result.getSuccess());
+			result = instance.write(new WriteRequest(0, 0, 0, 0));
+			assertEquals(false, result.getSuccess());
 
-            //WHAT SHOULD BE RESULT, true or false?  At the moment it will succeed and test will fail
-            result = instance.begin(new BeginRequest(0));
-            assertEquals(true, result.getSuccess());
+			// WHAT SHOULD BE RESULT, true or false? At the moment it will
+			// succeed and test will fail
+			result = instance.begin(new BeginRequest(0));
+			assertEquals(true, result.getSuccess());
 
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
+		} catch (IOException ex) {
+			fail(ex.getLocalizedMessage());
+		}
 
-      }
+	}
 
-    @Test
-    public void testSingle()
-      {
-        try
-          {
-            MVCC2PL instance = new MVCC2PL();
+	@Test
+	public void testSingle() {
+		try {
+			MVCC2PL instance = new MVCC2PL();
 
-            GenericSuccessResponse br = instance.begin(new BeginRequest(1));
-            assertEquals(true, br.getSuccess());
+			GenericSuccessResponse br = instance.begin(new BeginRequest(1));
+			assertEquals(true, br.getSuccess());
 
-            GenericSuccessResponse wr = instance.write(new WriteRequest(1, 0, "zero", 0));
-            assertEquals(true, wr.getSuccess());
+			GenericSuccessResponse wr = instance.write(new WriteRequest(1, 0,
+					"zero", 0));
+			assertEquals(true, wr.getSuccess());
 
-            ReadResponse rr = instance.read(new ReadRequest(1, 0, 0));
-            assertEquals(true, rr.getSuccess());
-            assertEquals("zero", (String) rr.getValue());
+			ReadResponse rr = instance.read(new ReadRequest(1, 0, 0));
+			assertEquals(true, rr.getSuccess());
+			assertEquals("zero", (String) rr.getValue());
 
-            GenericSuccessResponse cr = instance.commit(new CommitRequest(1));
-            assertEquals(true, cr.getSuccess());
+			GenericSuccessResponse cr = instance.commit(new CommitRequest(1));
+			assertEquals(true, cr.getSuccess());
 
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
+		} catch (IOException ex) {
+			fail(ex.getLocalizedMessage());
+		}
 
-      }
+	}
 
-    @Test
-    public void testSerial()
-      {
-        try
-          {
-            MVCC2PL instance = new MVCC2PL();
+	@Test
+	public void testSerial() {
+		try {
+			MVCC2PL instance = new MVCC2PL();
 
-            GenericSuccessResponse gsr = instance.begin(new BeginRequest(0));
-            assertEquals(true, gsr.getSuccess());
+			GenericSuccessResponse gsr = instance.begin(new BeginRequest(0));
+			assertEquals(true, gsr.getSuccess());
 
-            gsr = instance.write(new WriteRequest(0, 0, "zero", 0));
-            assertEquals(true, gsr.getSuccess());
+			gsr = instance.write(new WriteRequest(0, 0, "zero", 0));
+			assertEquals(true, gsr.getSuccess());
 
-            gsr = instance.write(new WriteRequest(0, 1, "one", 0));
-            assertEquals(true, gsr.getSuccess());
+			gsr = instance.write(new WriteRequest(0, 1, "one", 0));
+			assertEquals(true, gsr.getSuccess());
 
-            gsr = instance.commit(new CommitRequest(0));
-            assertEquals(true, gsr.getSuccess());
+			gsr = instance.commit(new CommitRequest(0));
+			assertEquals(true, gsr.getSuccess());
 
-            gsr = instance.begin(new BeginRequest(1));
-            assertEquals(true, gsr.getSuccess());
+			gsr = instance.begin(new BeginRequest(1));
+			assertEquals(true, gsr.getSuccess());
 
-            ReadResponse rr = instance.read(new ReadRequest(1, 1, 0));
-            assertEquals(true, rr.getSuccess());
-            assertEquals("one", (String) rr.getValue());
+			ReadResponse rr = instance.read(new ReadRequest(1, 1, 0));
+			assertEquals(true, rr.getSuccess());
+			assertEquals("one", (String) rr.getValue());
 
-            rr = instance.read(new ReadRequest(1, 0, 0));
-            assertEquals(true, rr.getSuccess());
-            assertEquals("zero", (String) rr.getValue());
+			rr = instance.read(new ReadRequest(1, 0, 0));
+			assertEquals(true, rr.getSuccess());
+			assertEquals("zero", (String) rr.getValue());
 
-            gsr = instance.commit(new CommitRequest(1));
-            assertEquals(true, gsr.getSuccess());
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
+			gsr = instance.commit(new CommitRequest(1));
+			assertEquals(true, gsr.getSuccess());
+		} catch (IOException ex) {
+			fail(ex.getLocalizedMessage());
+		}
 
-      }
+	}
 
-    @Test
-    public void testDeadlock()
-      {
-        try
-          {
-            MVCC2PL instance = new MVCC2PL();
+	@Test
+	public void testDeadlock() {
 
-            GenericSuccessResponse gsr;
-            ReadResponse rr;
+		Thread thread1 = new Thread(new Runnable() {
 
-            gsr = instance.begin(new BeginRequest(1));
-            assertEquals(true, gsr.getSuccess());
+			@Override
+			public void run() {
+				try {
+					GenericSuccessResponse gsr;
+					ReadResponse rr;
 
-            gsr = instance.begin(new BeginRequest(2));
-            assertEquals(true, gsr.getSuccess());
+					gsr = concurrentInstance.begin(new BeginRequest(1));
+					assertEquals(true, gsr.getSuccess());
 
-            rr = instance.read(new ReadRequest(1, 1, 0));
-            assertEquals(true, rr.getSuccess());
-            assertEquals(null, rr.getValue());
+					rr = concurrentInstance.read(new ReadRequest(1, 1, 0));
+					assertEquals(true, rr.getSuccess());
+					assertEquals(null, rr.getValue());
 
-            rr = instance.read(new ReadRequest(2, 2, 0));
-            assertEquals(true, rr.getSuccess());
-            assertEquals(null, rr.getValue());
+					Thread.sleep(2000);
 
-            gsr = instance.write(new WriteRequest(1, 2, "y1", 0));
-            assertEquals(true, gsr.getSuccess());
+					gsr = concurrentInstance.write(new WriteRequest(1, 2, "y1",
+							0));
+					assertEquals(true, gsr.getSuccess());
 
-            gsr = instance.write(new WriteRequest(2, 1, "x2", 0));
-            assertEquals(true, gsr.getSuccess());
+					Thread.sleep(2000);
 
-//            gsr = instance.commit(new CommitRequest(1));
-//            assertEquals(true, gsr.getSuccess());
+					gsr = concurrentInstance.commit(new CommitRequest(1));
+					assertEquals(true, gsr.getSuccess());
 
-//            gsr = instance.commit(new CommitRequest(2));
-//            assertEquals(true, gsr.getSuccess());
+				} catch (Exception ex) {
+					fail(ex.getLocalizedMessage());
+				}
+			}
+		});
 
-          } catch (IOException ex)
-          {
-            fail(ex.getLocalizedMessage());
-          }
-      }
-  }
+		Thread thread2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					GenericSuccessResponse gsr;
+					ReadResponse rr;
+
+					Thread.sleep(1000);
+
+					gsr = concurrentInstance.begin(new BeginRequest(2));
+					assertEquals(true, gsr.getSuccess());
+
+					rr = concurrentInstance.read(new ReadRequest(2, 2, 0));
+					assertEquals(true, rr.getSuccess());
+					assertEquals(null, rr.getValue());
+
+					Thread.sleep(2000);
+
+					gsr = concurrentInstance.write(new WriteRequest(2, 1, "x2",
+							0));
+					assertEquals(true, gsr.getSuccess());
+
+					Thread.sleep(10000); // I make it wait long to make sure
+											// that I give time to the thread1
+
+					gsr = concurrentInstance.commit(new CommitRequest(2));
+					assertEquals(false, gsr.getSuccess());
+
+				} catch (Exception ex) {
+					fail(ex.getLocalizedMessage());
+				}
+			}
+		});
+
+		thread1.start();
+		thread2.start();
+
+		try {
+			// thread1.join();
+			thread2.join();
+		} catch (InterruptedException ex) {
+			fail(ex.getLocalizedMessage());
+		}
+	}
+}
