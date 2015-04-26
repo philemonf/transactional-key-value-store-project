abdb08323e35afd9fac1a922b87e16a98a5bc72b
Andy Roulin <andy.roulin@epfl.ch>
2015-04-26 19:39:02 +0200

    Extract PrefixedKey class

--- src/ch/epfl/tkvs/transactionmanager/versioningunit/VersioningUnitMVTO.java
+++ src/ch/epfl/tkvs/transactionmanager/versioningunit/VersioningUnitMVTO.java
@@ -1,22 +1,59 @@
 package ch.epfl.tkvs.transactionmanager.versioningunit;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import ch.epfl.tkvs.keyvaluestore.KeyValueStore;
 
 
 public enum VersioningUnitMVTO implements IVersioningUnit {
     instance;
 
+    private int testing_max_xact = 0;
+
     // The Timestamp on which a Serializable key was last read
     private Map<Serializable, Integer> RTS;
     // The Timestamp on which a Serializable key was last written
     private Map<Serializable, Integer> WTS;
+    // The different versions of a given key in descending order of timestamp
+    private Map<Serializable, List<Version>> versions;
+
+    private class Version {
+
+        // key to access this version in the KVStore
+        Serializable key;
+        // Timestamp of this version's write
+        int WTS;
+    }
 
     @Override
     public void init() {
+        // TODO Init and Flush KVStore ?
+        KeyValueStore.instance.clear();
+
+        // Flush data structures
         RTS = new ConcurrentHashMap<Serializable, Integer>();
         WTS = new ConcurrentHashMap<Serializable, Integer>();
+    }
+
+    /**
+     * Tell the versioning unit about a new transaction
+     * @param xact the ID or timestamp of the transaction If the xact is -1, then the VU should generate a xact (testing
+     * purpose only)
+     */
+    public int begin_transaction(int xact) {
+
+        // If we are testing the VU
+        if (xact == -1) {
+            return ++testing_max_xact;
+        }
+
+        // Initialize data structures for the new transaction
+        // TODO
+
+        return xact;
     }
 
     @Override
@@ -40,11 +77,6 @@ public enum VersioningUnitMVTO implements IVersioningUnit {
     public void stopNow() {
         // TODO Auto-generated method stub
 
-    }
-
-    public int begin_transaction(int xact) {
-        // TODO Auto-generated method stub
-        return 0;
     }
 
     public void abort(int xact) {
