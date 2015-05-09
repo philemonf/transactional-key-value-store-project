85b54da041274061bd334b2e3ca3657ddf3ef9b8
Sachin Basil John <sachinbjohn@gmail.com>
2015-05-09 23:06:57 +0200

    fixed JSONConstructor in DeadlockInfoMessage

--- src/ch/epfl/tkvs/transactionmanager/communication/DeadlockInfoMessage.java
+++ src/ch/epfl/tkvs/transactionmanager/communication/DeadlockInfoMessage.java
@@ -7,58 +7,62 @@ import java.util.Set;
 import ch.epfl.tkvs.transactionmanager.communication.utils.Base64Utils;
 import ch.epfl.tkvs.transactionmanager.lockingunit.DeadlockGraph;
 
+
 public class DeadlockInfoMessage extends Message {
 
-	@JSONAnnotation(key = JSONCommunication.KEY_FOR_MESSAGE_TYPE)
-	public static final String MESSAGE_TYPE = "deadlock_message";
+    @JSONAnnotation(key = JSONCommunication.KEY_FOR_MESSAGE_TYPE)
+    public static final String MESSAGE_TYPE = "deadlock_message";
 
-	@JSONAnnotation(key = JSONCommunication.KEY_FOR_DEAD_LOCK_GRAPH)
-	private String encodedGraph;
+    @JSONAnnotation(key = JSONCommunication.KEY_FOR_DEAD_LOCK_GRAPH)
+    private String encodedGraph;
 
-	@JSONConstructor
-	public DeadlockInfoMessage(int localHash, DeadlockGraph graph, Set<Integer> activeTransactions) throws IOException {
-		encodedGraph = Base64Utils.convertToBase64(new DeadlockInfo(localHash, graph, activeTransactions));
-	}
+    public DeadlockInfoMessage(int localHash, DeadlockGraph graph, Set<Integer> activeTransactions) throws IOException {
+        encodedGraph = Base64Utils.convertToBase64(new DeadlockInfo(localHash, graph, activeTransactions));
+    }
 
-	/**
-	 * Returns the dead lock graph contained in the message.
-	 * 
-	 * @return the dead lock graph if available or null (be careful)
-	 * @throws ClassNotFoundException
-	 *             in case the DeadlockGraph class is not in your classpath
-	 * @throws IOException
-	 *             in case of decoding problem
-	 */
-	public DeadlockInfo getGraph() throws ClassNotFoundException, IOException {
-		if (encodedGraph == null) {
-			return null;
-		}
+    @JSONConstructor
+    public DeadlockInfoMessage(String encodedGraph) {
+        this.encodedGraph = encodedGraph;
+    }
 
-		return (DeadlockInfo) Base64Utils.convertFromBase64(encodedGraph);
-	}
+    /**
+     * Returns the dead lock graph contained in the message.
+     * 
+     * @return the dead lock graph if available or null (be careful)
+     * @throws ClassNotFoundException in case the DeadlockGraph class is not in your classpath
+     * @throws IOException in case of decoding problem
+     */
+    public DeadlockInfo getGraph() throws ClassNotFoundException, IOException {
+        if (encodedGraph == null) {
+            return null;
+        }
 
-	public class DeadlockInfo implements Serializable {
-		private static final long serialVersionUID = 1L;
-		private int localHash;
-		private DeadlockGraph graph;
-		private Set<Integer> activeTransactions;
+        return (DeadlockInfo) Base64Utils.convertFromBase64(encodedGraph);
+    }
 
-		public DeadlockInfo(int localHash, DeadlockGraph graph, Set<Integer> activeTransactions) {
-			this.localHash = localHash;
-			this.graph = graph;
-			this.activeTransactions = activeTransactions;
-		}
+    public class DeadlockInfo implements Serializable {
 
-		public int getLocalHash() {
-			return localHash;
-		}
+        private static final long serialVersionUID = 1L;
+        private int localHash;
+        private DeadlockGraph graph;
+        private Set<Integer> activeTransactions;
 
-		public DeadlockGraph getGraph() {
-			return graph;
-		}
-		
-		public Set<Integer> getActiveTransactions() {
-			return activeTransactions;
-		}
-	}
+        public DeadlockInfo(int localHash, DeadlockGraph graph, Set<Integer> activeTransactions) {
+            this.localHash = localHash;
+            this.graph = graph;
+            this.activeTransactions = activeTransactions;
+        }
+
+        public int getLocalHash() {
+            return localHash;
+        }
+
+        public DeadlockGraph getGraph() {
+            return graph;
+        }
+
+        public Set<Integer> getActiveTransactions() {
+            return activeTransactions;
+        }
+    }
 }
