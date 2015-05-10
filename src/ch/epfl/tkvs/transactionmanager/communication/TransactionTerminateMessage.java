8e83523ac7b29d28b51c16434a093827887116f6
philemonf <philemon.favrod@epfl.ch>
2015-05-10 13:23:47 +0200

    forgot to use base64

--- src/ch/epfl/tkvs/transactionmanager/communication/TransactionTerminateMessage.java
+++ src/ch/epfl/tkvs/transactionmanager/communication/TransactionTerminateMessage.java
@@ -3,26 +3,33 @@ package ch.epfl.tkvs.transactionmanager.communication;
 import static ch.epfl.tkvs.transactionmanager.communication.JSONCommunication.KEY_FOR_MESSAGE_TYPE;
 import static ch.epfl.tkvs.transactionmanager.communication.JSONCommunication.KEY_FOR_TRANSACTION_ID;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.LinkedList;
+
+import ch.epfl.tkvs.transactionmanager.communication.utils.Base64Utils;
 
 public class TransactionTerminateMessage extends Message {
 	@JSONAnnotation(key = KEY_FOR_MESSAGE_TYPE)
 	public static final String MESSAGE_TYPE = "xact_terminate_message";
 	
 	@JSONAnnotation(key = KEY_FOR_TRANSACTION_ID)
-	private LinkedList<Integer> transactionIds;
+	private String encodedTids;
 	
 	@JSONConstructor
-	public TransactionTerminateMessage(LinkedList<Integer> tids) {
-		this.transactionIds = tids;
+	public TransactionTerminateMessage(LinkedList<Integer> tids) throws IOException {
+		encodedTids = Base64Utils.convertToBase64(tids);
 	}
 	
-	public TransactionTerminateMessage(int transactionId) {
-		this.transactionIds = new LinkedList<Integer>();
-		this.transactionIds.add(transactionId);
+	public TransactionTerminateMessage(int transactionId) throws IOException {
+		this(new LinkedList<Integer>(Arrays.asList(transactionId)));
 	}
 
 	public LinkedList<Integer> getTransactionIds() {
-		return transactionIds;
+		try {
+			return (LinkedList<Integer>) Base64Utils.convertFromBase64(encodedTids);
+		} catch (Exception e) {
+			return new LinkedList<Integer>();
+		}
 	}
 }
