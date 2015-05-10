5f74210f64a0d19fc8ecf6ed7872f9bdce4e7984
philemonf <philemon.favrod@epfl.ch>
2015-05-10 09:24:55 +0200

    comments + log

--- src/ch/epfl/tkvs/yarn/Client.java
+++ src/ch/epfl/tkvs/yarn/Client.java
@@ -143,7 +143,7 @@ public class Client {
                 hist.add(input);
 
                 System.out.println("Running test client...\n");
-                runTestCase(S2PLSystemTest.class);
+                runTestCase(MVTOSystemTest.class);
                 UserTransaction.log.writeToHDFS("C");
                 // log.info("Running example client program...");
                 // new UserClient().run();
