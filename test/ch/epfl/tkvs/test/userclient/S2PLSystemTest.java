7deeee61e457eeec72008d3ecda81bc747fa769a
egeyar <egeyar@gmail.com>
2015-05-10 23:54:22 +0200

    corrected S2PLSystemTest.java

--- test/ch/epfl/tkvs/test/userclient/S2PLSystemTest.java
+++ test/ch/epfl/tkvs/test/userclient/S2PLSystemTest.java
@@ -11,7 +11,7 @@ public class S2PLSystemTest extends UserClientScheduledTest {
         MyKey x = freshKey("x", 0);
         MyKey y = freshKey("y", 1);
         TransactionExecutionCommand schedule[][] = {
-        /* T1 */{ BEGIN__(0, t), READ(x, "00"), W(y, "y1", t), COMMIT____(t) },
+        /* T1 */{ BEGIN__(0, t), READ(x, "00"), W(y, "y1", f), COMMIT____(f) },
         /* T2 */{ BEGIN__(1, t), READ(y, "00"), W(x, "x2", t), COMMIT____(t) } };
         execute(schedule, x, y);
 
